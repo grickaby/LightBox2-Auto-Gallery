ac2eb99b4a643d43540671dc501ab0a8ef14ef5c
Geoffrey Rickaby <grickaby@gmail.com>
2015-07-14 09:55:53 -0500

    added captions
    it will now pull the file name and use that as a caption if you so wish.

--- include/gallery.php
+++ include/gallery.php
@@ -3,7 +3,7 @@
 // LightBox2 Auto Gallery
 // A light weight PHP and LigthBox2 powered Auto Gallery Script
 // 
-// Version 1.0.0
+// Version 1.1.0
 // Author: Geoffrey Rickaby
 // URL: http://github.com/grickaby/LightBox2-Auto-Gallery
 // 
@@ -14,6 +14,10 @@
 // jQuery v 1.11.0+
 // Ligthbox2 v 2.7.1+ 
 // For thumbnail styleing use built in style sheet
+// 
+// Updates:
+// 1.1.0 - The plugin now pulls captions from the file name
+// 1.0.0 - Initial Release
 //======================================================================
 
 //Config Me
@@ -21,15 +25,24 @@ $galleryName = "lawncare"; //Set the name of the gallery
 $maxWidth = 200; //Set the max width for the thumbnails
 $maxHeight = 200; // Set the max height for the thumbnails
 $location = "gallery/*"; //Location of the images
+$captions = true; //Do you want captions? true or false
+$separator = "_"; //If you want captions, put the character here that separates the words.
 $allowed = array('gif', 'png', 'jpg', 'jpeg'); //Allowed file extensions
 
-$i = 0;
+//Run Me
+$i = 1;
 $images = glob($location);
 foreach ($images as $image) :
-    $i++;
     $file = pathinfo($image, PATHINFO_EXTENSION);
+    if($captions === true) {
+        $caption = str_replace($separator, " ", pathinfo($image, PATHINFO_FILENAME));
+        $the_caption = "data-title='". $caption . "'";
+    } else {
+        $the_caption = null;
+    }
+    
     if (in_array($file, $allowed)) {
-        echo "<a href='" . $image . "' data-lightbox='" . $galleryName . "'><img class='lb-thumb' data-lightbox='image-" . $i . "' src='" . $image . "' width='" . $maxWidth . "' height='" . $maxHeight . "' /></a>";
+        echo "<a href='" . $image . "' data-lightbox='" . $galleryName . "' " . $the_caption . "><img class='lb-thumb' data-lightbox='image-" . $i++ . "' src='" . $image . "' width='" . $maxWidth . "' height='" . $maxHeight . "' /></a>";
     }
 endforeach;
 ?>
